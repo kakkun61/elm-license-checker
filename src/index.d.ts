1d759826319e5fcb7006f364796b795e87a0118b
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2020-01-10 22:12:48 +0900

    make to get redundant fields

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,20 +9,10 @@
 import * as lc from "license-checker";
 
 /**
- * Options struct for the init() function
- */
-export interface InitOpts {
-  /**
-   * Path to start checking dependencies from
-   */
-  start: string
-}
-
-/**
  * Run the license check
  * @param opts specifies the path to the module to check dependencies of
  */
 export function init(
-  opts: InitOpts,
+  opts: lc.InitOpts,
   callback: (err: Error, ret: lc.ModuleInfos) => void
 ): void;
